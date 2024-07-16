0f22fe2b796b8eee77ea566ae68eb265f74549e8
pavankumar-parimi <pavankumarpev@gmail.com>
2024-07-16 01:55:31 -0400

    created category-menu and category-item component for menu screen.

---
+++ src/components/category-menu/category-menu.component.jsx
@@ -0,0 +1,17 @@
+import './category-menu.styles.scss'
+import CategoryItem from '../category-item/category-item.component'
+
+const CategoryMenu = ({categories}) => {
+
+    return (
+        <div className="category-menu-container">
+            {
+            categories.map((category) => (
+                <CategoryItem key = {category.id} category = {category} />
+            ))
+            }
+        </div>
+    )
+}
+
+export default CategoryMenu;
